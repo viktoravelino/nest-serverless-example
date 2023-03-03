81ae28a5eb8a137fcc186e1b674761d86281d70a
Viktor Avelino <viktor.avelino@gmail.com>
2023-03-03 13:46:42 -0500

    fix: swagger

--- src/serverless.ts
+++ src/serverless.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import serverlessExpress from '@vendia/serverless-express';
 import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
@@ -7,8 +8,17 @@ let server: Handler;
 
 async function bootstrap(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  await app.init();
 
+  const config = new DocumentBuilder()
+    .setTitle('Fake API')
+    .setDescription('Returns fake data')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
   return serverlessExpress({ app: expressApp });
 }
@@ -18,6 +28,14 @@ export const handler: Handler = async (
   context: Context,
   callback: Callback,
 ) => {
+  if (event.path === '/api') {
+    event.path = '/api/';
+  }
+
+  event.path = event.path.includes('swagger-ui')
+    ? `/api${event.path}`
+    : event.path;
+
   server = server ?? (await bootstrap());
   return server(event, context, callback);
 };
